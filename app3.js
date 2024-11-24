53f02431655e2d39ddad10a94339957451745d1f
EchoSwift-12 <amtulrahmansadiya@gamil.com>
2024-11-24 21:57:02 +0530

    reallifeexample

--- app3.js
+++ app3.js
@@ -13,6 +13,29 @@ myPromise
     .then((result) => {
         console.log("Fulfilled");
     })
-    .catch((result) =>{
-        console.log("rejected");
+    .catch((result) => {
+        // console.log("rejected");
     })
+
+// Real life Example    
+const fetchData = new Promise((resolve, reject) => {
+    let dataLoaded = false;
+
+    setTimeout(() => {
+        if (dataLoaded) {
+            resolve("Data fetched!");
+        }
+        else {
+            reject("Failed to fetch data!");
+        }
+    },
+        2000);
+})
+
+fetchData
+    .then((message) => {
+        console.log(message);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
