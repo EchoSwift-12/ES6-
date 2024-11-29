7af1c7197d7fa315fb8622df77040aa591f6a08a
EchoSwift-12 <amtulrahmansadiya@gamil.com>
2024-11-30 01:01:25 +0530

    async await q

--- tasks.js
+++ tasks.js
@@ -3,25 +3,25 @@
 // //  The task should randomly succeed or fail after 2 seconds. 
 // // Use .then() and .catch() to handle the result.
 
-// const fetch = new Promise((resolve, reject) => {
-//     const isSuccessful = Math.random() > 0.5;
-//     setTimeout(() => {
-//         if (isSuccessful) {
-//             resolve("successfully fetched");
-//         }
-//         else {
-//             reject("failed to reject");
-//         }
-//     }, 2000);
-// })
-// fetch
-//     .then((message) => {
-//         console.log(message);
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     })
-// fetch;
+const fetch = new Promise((resolve, reject) => {
+    const isSuccessful = Math.random() > 0.5;
+    setTimeout(() => {
+        if (isSuccessful) {
+            resolve("successfully fetched");
+        }
+        else {
+            reject("failed to reject");
+        }
+    }, 2000);
+})
+fetch
+    .then((message) => {
+        console.log(message);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+fetch;
 
 // // Task 2: Promise Chaining
 // // Create three promises that simulate different stages of a process 
@@ -29,176 +29,176 @@
 // // Chain them together using .then(). If any promise fails, 
 // // catch the error and display an error message.
 
-// const chaining = new Promise((resolve, reject) => {
-//     let resolves = math.random > 0.5; //simulate random success/failure
-//     setTimeout(() => {
-//         if (resolves)
-//             resolve("order placing");
-//         else
-//             reject("your order is rejected");
-//     }, 1000);
-// })
-// chaining
-//     .then((message) => {
-//         console.log(message);
-//         return new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                 resolve("order processing");
-//             }, 1000);
-//         })
-//     })
+const chaining = new Promise((resolve, reject) => {
+    let resolves = math.random > 0.5; //simulate random success/failure
+    setTimeout(() => {
+        if (resolves)
+            resolve("order placing");
+        else
+            reject("your order is rejected");
+    }, 1000);
+})
+chaining
+    .then((message) => {
+        console.log(message);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("order processing");
+            }, 1000);
+        })
+    })
 
-//     .then((message) => {
-//         console.log(message);
-//         return new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                 resolve("order shipping");
-//             }, 1000);
-//         })
-//     })
-//     .then((message) => {
-//         console.log(message);
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     })
-// chaining;
+    .then((message) => {
+        console.log(message);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("order shipping");
+            }, 1000);
+        })
+    })
+    .then((message) => {
+        console.log(message);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+chaining;
 
 // // method 2
-// const placeOrder = () => {
-//     Promise.resolve("Order Placed");
-// }
-// const processOrder = () => {
-//     Promise.resolve("Order Processed");
-// }
-// const shipOrder = () => {
-//     Promise.resolve("Order Shipped");
-// }
-// placeOrder()
-//     .then((message) => {
-//         console.log(message);
-//         return processOrder();
-//     })
-//     .then((message) => {
-//         console.log(message);
-//         return shipOrder();
-//     })
-//     .then((message) => {
-//         console.log(message)
-//     })
-//     .catch((error) => {
-//         console.log(error)
-//     })
+const placeOrder = () => {
+    Promise.resolve("Order Placed");
+}
+const processOrder = () => {
+    Promise.resolve("Order Processed");
+}
+const shipOrder = () => {
+    Promise.resolve("Order Shipped");
+}
+placeOrder()
+    .then((message) => {
+        console.log(message);
+        return processOrder();
+    })
+    .then((message) => {
+        console.log(message);
+        return shipOrder();
+    })
+    .then((message) => {
+        console.log(message)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 
 // // Task 3: Using Promise.all
 // // You have two asynchronous tasks: one to fetch a list of users and another to fetch a list of posts.
 // //  Use Promise.all to execute both promises simultaneously and log the results when both are fulfilled.
 
-// const fetchListUsers = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve("list of users fetched");
-//     }, 1500);
-// })
+const fetchListUsers = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("list of users fetched");
+    }, 1500);
+})
 
-// const fetchListPost = new Promise((resolve) => {
-//     setTimeout(() => {
-//         resolve("list of posts fetched");
-//     }, 2000);
-// })
+const fetchListPost = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("list of posts fetched");
+    }, 2000);
+})
 
-// Promise.all([fetchListUsers, fetchListPost])
-//     .then((message) => {
-//         console.log("all tasks completed");
-//         console.log(message);
-//     })
-//     .catch((error) => {
-//         console.log("an error occurred", error);
-//     });
+Promise.all([fetchListUsers, fetchListPost])
+    .then((message) => {
+        console.log("all tasks completed");
+        console.log(message);
+    })
+    .catch((error) => {
+        console.log("an error occurred", error);
+    });
 
 // //  Task 4: Fetch API with Promises
 // //  Write a function that fetches data from a public API 
 // //  (like JSONPlaceholder) and logs the fetched data to the console. 
 // //  Handle any errors that occur during the fetch using .catch().
 
-// const fetchAPI = () => {
-//     fetch("https://jsonplaceholder.typicode.com/posts/1")
-//         .then((response) => {
-//             if (!response.ok) {
-//                 throw new Error(`HTTPS error! Status: ${response.status}`);
-//             }
-//             return response.json();
-//         })
+const fetchAPI = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTPS error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
 
-//         .then((message) => {
-//             console.log("fetched data", message);
-//         })
-//         .catch((error) => {
-//             console.log("Error Occurred", error);
-//         });
-// }
-// fetchAPI;
+        .then((message) => {
+            console.log("fetched data", message);
+        })
+        .catch((error) => {
+            console.log("Error Occurred", error);
+        });
+}
+fetchAPI;
 
 // // Task 5: Implement Promise.allSettled
 // // Create two promises: one that resolves and another that rejects.
 // //  Use Promise.allSettled() to wait for both promises to finish and log their results.
 
-// const fulfil = Promise.resolve("Rsolved promise");
-// const reject = Promise.reject("rejects promise");
-// Promise.allSettled([fulfil, reject])
-//     .then((results) => {
-//         console.log(results);
-//     })
+const fulfil = Promise.resolve("Rsolved promise");
+const reject = Promise.reject("rejects promise");
+Promise.allSettled([fulfil, reject])
+    .then((results) => {
+        console.log(results);
+    })
 
 // // Task 6: Error Handling in Chaining
 // // Write a promise chain with multiple .then() calls. 
 // // In one of the .then() calls, simulate an error by rejecting the promise. 
 // // Use .catch() to handle the error and log it to the console.
-// const promiseChain = Promise.resolve("start")
-//     .then((result) => {
-//         console.log(result);
-//         return "step1";
-//     })
-//     .then((result) => {
-//         console.log(result);
-//         throw new Error("Something went wrong");
-//     })
-//     .then((result) => {
-//         console.log(result);
-//     })
-//     .catch((error) => {
-//         console.log("error caught", error.message);
-//     })
+const promiseChain = Promise.resolve("start")
+    .then((result) => {
+        console.log(result);
+        return "step1";
+    })
+    .then((result) => {
+        console.log(result);
+        throw new Error("Something went wrong");
+    })
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.log("error caught", error.message);
+    })
 
 // //  Task 7: Delay Function with Promises
 // //  Write a function delay(ms) that returns a promise which resolves 
 // //  after a given number of milliseconds.
 // // Test it by delaying a console.log message for 2 seconds.
 
-// const delay = (ms) => {
-//     return new Promise((resolve) =>
-//         setTimeout(resolve, ms));
-// }
-// delay(2000)
-//     .then(() => {
-//         console.log("message delayed for 2 seconds");
-//     })
+const delay = (ms) => {
+    return new Promise((resolve) =>
+        setTimeout(resolve, ms));
+}
+delay(2000)
+    .then(() => {
+        console.log("message delayed for 2 seconds");
+    })
 
-// // Task 8: Timer Using Promises
-// // Use a setTimeout inside a promise to simulate a timer. 
-// // The promise should resolve after 3 seconds with a message, 
-// // and the result should be logged using .then().    
+// Task 8: Timer Using Promises
+// Use a setTimeout inside a promise to simulate a timer. 
+// The promise should resolve after 3 seconds with a message, 
+// and the result should be logged using .then().    
 
-// function timer() {
-//     return new Promise((resolve) => {
-//         setTimeout(() => {
-//             resolve("timer completed");
-//         }, 3000);
-//     })
-// }
-// timer().then((message) => {
-//     console.log(message);//timer completed
-// })
+function timer() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("timer completed");
+        }, 3000);
+    })
+}
+timer().then((message) => {
+    console.log(message);//timer completed
+})
 
 // Task 9: Fetch Data with Dynamic URL
 // Write a promise-based function that accepts a dynamic URL as a parameter
---
+++ tasks2.js
@@ -0,0 +1,116 @@
+// -------------- Basic Tasks-----------------
+//1.Simple Await
+//Write an async function that resolves a promise with the message "Hello, Async!". 
+// Use await to get the result and log it to the console.
+const asyncFunc = async () => {
+    // the below line, creates a promise that resolves immediately 
+    // with th value "Hello async!".
+    const promise = Promise.resolve("Hello, async");
+    // await promise waits for the promise to resolve and 
+    // retrieves  its resolved value.
+    const result = await promise;
+    // the resolved value is stored in result
+    console.log(result);
+}
+asyncFunc();
+
+//2.Error Handling with try...catch
+//Create a function that rejects a promise with an error message like "Something went wrong". 
+// Use try...catch to handle the error.
+async function tryCatchFunc() {
+    try {
+        result = await  Promise.reject("Something went wrong!");
+    }
+    catch (error) {
+        console.error("error:", error);
+    }
+}
+tryCatchFunc();
+
+//--------------- Intermediate Tasks----------------------
+// Sequential Execution
+// Write an async function that:
+// Resolves a promise with "Step 1 completed!" and logs it.
+// Waits for another promise to resolve with "Step 2 completed!" and logs it.
+// Waits for a final promise to resolve with "Step 3 completed!" and logs it.
+const asyncFunction = async () => {
+
+    const step1 = await Promise.resolve("Step 1 Completed");
+    console.log(step1);
+
+    const step2 = await Promise.resolve("Step 2 completed");
+    console.log(step2);
+
+    const step3 = await Promise.resolve("Step 3 completed");
+    console.log(step3);
+}
+asyncFunction();
+
+// Fetch Data
+// Use the fetch API with await to get data from
+//  https://jsonplaceholder.typicode.com/posts.
+// Parse the response as JSON and log the first post's title.
+// Handle errors gracefully.
+
+const fetchURL = async () => {
+    data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const info = await data.json();
+    console.log(info);
+}
+fetchURL()
+    .then((message) => {
+        console.log("done");
+    })
+    .catch((error) => {
+        console.log("error:", error.message);
+    });
+
+// good method
+// using try catch metch with async await
+async function fetchURL2() {
+    try {
+        data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+        info = await data.json();
+        console.log(info);
+    }
+    catch (error) {
+        console.log("error", error.message);
+    }
+}
+fetchURL2();
+
+//---------------- Advanced Tasks-------------------
+// Parallel Execution
+// Create two promises:
+// The first resolves after 2 seconds with "Promise 1 resolved".
+// The second resolves after 3 seconds with "Promise 2 resolved".
+// Use Promise.all with async/await to wait for both and log the results.
+const prom1 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("promise 1 resolved");
+    }, 2000);
+})
+
+const prom2 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("promise 2 resolved");
+    }, 3000);
+})
+
+const waitForBoth = async() =>{
+    try{
+        const results = await Promise.all([prom2, prom2]);
+        console.log("results", results)
+    }
+    catch(error){
+        console.log("error:", error.message);
+    }
+}
+waitForBoth();
+
+
+// Delay Function
+// Write a delay function that takes a number of milliseconds as an argument and returns a promise that resolves after that delay.
+// Use it in an async function to log "Start", wait 3 seconds, and then log "End".
+
+
